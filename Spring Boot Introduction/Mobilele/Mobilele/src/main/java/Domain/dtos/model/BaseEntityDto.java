1fc3e0745c8e788a5ce8209947d446c2a0f1c739
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-01-24 19:27:03 +0200

    Add files via upload

---
+++ Spring Boot Introduction/Mobilele/Mobilele/src/main/java/Domain/dtos/model/BaseEntityDto.java
@@ -0,0 +1,17 @@
+package Domain.dtos.model;
+
+public abstract class BaseEntityDto {
+    private String id;
+
+    public BaseEntityDto() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public BaseEntityDto setId(String id) {
+        this.id = id;
+        return this;
+    }
+}

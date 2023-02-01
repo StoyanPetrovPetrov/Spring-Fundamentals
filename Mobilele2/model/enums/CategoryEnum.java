a5acca351a0b84f48b9947a284b0759ddd1ec0c6
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-02-01 18:37:42 +0200

    Add files via upload

---
+++ Mobilele2/model/enums/CategoryEnum.java
@@ -0,0 +1,8 @@
+package com.example.mobilele2.model.enums;
+
+public enum CategoryEnum {
+    CAR,
+    BUS,
+    TRUCK,
+    MOTORCYCLE
+}
---
+++ Mobilele2/model/enums/EngineEnum.java
@@ -0,0 +1,8 @@
+package com.example.mobilele2.model.enums;
+
+public enum EngineEnum {
+    GASOLINE,
+    DIESEL,
+    ELECTRIC,
+    HYBRID
+}

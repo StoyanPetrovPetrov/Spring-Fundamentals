ab5c7d553fdf1583a699b2a22b73c8f419df9781
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-02-08 19:15:52 +0200

    Add files via upload

---
+++ Mobilele2/repository/ModelRepository.java
@@ -0,0 +1,9 @@
+package com.example.mobilele2.repository;
+
+import com.example.mobilele2.model.entity.ModelEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ModelRepository extends JpaRepository<ModelEntity, Long> {
+}

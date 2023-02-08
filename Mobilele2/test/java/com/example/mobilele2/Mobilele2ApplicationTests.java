8d41ae41a5ca9ab71bdc10bfea5e2fd60c3a6cd1
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-02-08 19:20:49 +0200

    Add files via upload

---
+++ Mobilele2/test/java/com/example/mobilele2/Mobilele2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mobilele2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mobilele2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

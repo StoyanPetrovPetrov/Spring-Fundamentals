2c6971ee14d8d39aa6d4837b6b92819fc437722e
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-02-08 19:19:47 +0200

    Add files via upload

---
+++ Mobilele2/Mobilele2Application.java
@@ -0,0 +1,13 @@
+package com.example.mobilele2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mobilele2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mobilele2Application.class, args);
+    }
+
+}

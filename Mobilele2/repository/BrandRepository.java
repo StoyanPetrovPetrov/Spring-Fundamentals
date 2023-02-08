fa3714ae95c65bb0fff1490db17a54020d4adcf9
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-02-08 19:15:30 +0200

    Create BrandRepository.java

---
+++ Mobilele2/repository/BrandRepository.java
@@ -0,0 +1,9 @@
+package com.example.mobilele2.repository;
+
+import com.example.mobilele2.model.entity.BrandEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<BrandEntity, Long> {
+}

38dc82ebb7723d63c8317a244c1ff0e740d8a32c
Stoyan Petrov <stoyanpetrov388@yahoo.com>
2023-02-08 19:16:44 +0200

    Add files via upload

---
+++ Mobilele2/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.mobilele2.repository;
+
+import com.example.mobilele2.model.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+}
